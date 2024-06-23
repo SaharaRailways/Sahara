e870ccc78ee1692dd70e52a54392535b1c2da23a
QuickPlayz <94027748+QuickPlayz@users.noreply.github.com>
2024-06-23 15:49:26 -0400

    Added resturaunt beta code

---
+++ Resturaunt/Ordering.lua
@@ -0,0 +1,45 @@
+tableList = {}
+tableList[item1] = {1, 5, 6}
+tableList[item2] = {}
+tableList[item3] = {}
+tableList[item4] = {}
+tableList[item5] = {}
+tableList[item6] = {}
+tableList[item7] = {}
+tableList[item8] = {}
+tableList[item9] = {}
+tableList[item10] = {}
+tableList[item11] = {}
+tableList[item12] = {}
+tableList[item13] = {}
+tableList[item14] = {}
+tableList[item15] = {}
+
+
+local payment = rs.getInput("back")
+local menuItem = rs.getAnalogInput("right")
+local table1 = peripheral.wrap("minecraft:barrel_0")
+local storage = peripheral.wrap("minecraft:chest_0")
+
+local function getTableBySignal(signal)
+    local tableName = "item" .. signal
+    return tableList[tableName]
+end
+
+local function getOrder()
+    for i,v in ipairs(getTableBySignal(menuItem)) do
+        storage.pushItems(peripheral.getName(table1), v , 1)
+        sleep(7)
+    end
+end
+
+print("Startup Complete")
+while true do
+    sleep(0.5)
+    if (rs.getInput("back")) then
+        print("Getting Order")
+        getOrder()
+        sleep(2)
+        print("Got Order")
+    end
+end
