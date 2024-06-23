516896841016096d35ed278d96a9620a1cf5c424
QuickPlayz <94027748+QuickPlayz@users.noreply.github.com>
2024-06-23 16:22:25 -0400

    added a func

---
+++ Misc/loggingClient.lua
@@ -0,0 +1,6 @@
+-- Things to work on
+-- create a library that players can import that sends a rednet message to a logging server
+
+function Logger(cID = computerID(), message):
+
+end
---
+++ Misc/loggingServer.lua
@@ -0,0 +1,3 @@
+-- Things to work on
+-- The logging server will take messages and make a text file log 
+
