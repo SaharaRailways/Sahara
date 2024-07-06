f8ca5104124544d4ec41acbbe35d8e35264c9762
Chanied <132589948+WolfZoned@users.noreply.github.com>
2024-07-06 11:42:30 -0400

    test3

--- common/totAPI.lua
+++ common/totAPI.lua
@@ -147,7 +147,7 @@ function tot.createInputBox(x,y,len,def,BC,TC)--Create an input box "object"(A t
     end
     return InputBox
 end
-function tcraete()
+function tot.createDropwodn()
 end
 function tot.printReceipt(playerName, receipt, priceList, pageHeader)
     local printer = peripheral.find("printer")
