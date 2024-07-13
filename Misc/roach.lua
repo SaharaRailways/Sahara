40241a38e0430bdc961256276274c637621e01a5
Chanied <132589948+WolfZoned@users.noreply.github.com>
2024-07-13 11:04:10 -0400

    started rewriting the code
    Co-authored-by: asosuhuj344@gmail.com <asosuhuj344@gmail.com>

--- Misc/roach.lua
+++ Misc/roach.lua
@@ -20,88 +20,38 @@ function events.entity_init()
 end
 
 --tick event, called 20 times per second
-local roach = true
+local isFollowing = true
+local head = models.model.root.Head
 local roaches = models.model.root.Head.roach
-local xOffset = 0
-local yOffset = 0
-local zOffset = 0
-local oldPos = vec(0,0,0)
-local sPos = vec(0,0,0)
-local curPos = vec(0,0,0)
-local sRot = vec(0,0,0)
-local curRot = vec(0,0,0)
-local myKey = keybinds:newKeybind("Name", "key.keyboard.u", false)
-local secKey = keybinds:newKeybind("Name", "key.keyboard.y", false)
-local thirdKey = keybinds:newKeybind("Name", "key.keyboard.h", false)
-local fourKey = keybinds:newKeybind("Name", "key.keyboard.g", false)
-local fiveKey = keybinds:newKeybind("Name", "key.keyboard.j", false)
-local sixKey = keybinds:newKeybind("Name", "key.keyboard.b", false)
-local sevenKey = keybinds:newKeybind("Name", "key.keyboard.n", false)
-myKey:setOnPress(function()
-  if roach then
-    oldPos = roaches:partToWorldMatrix():apply():mul(16,16,16)
-    oldRot = player:getRot()
-  end
-  roach = not roach
-  if roach then
-    --print(roaches:getPos())
-    roaches:setPos(0,0,0)
-    roaches:setRot(0,0,0)
-    roaches:moveTo(models.model.root.Head)
-  else
-    roaches:setPos(oldPos-roaches:getPivot())
-    roaches:setRot(-oldRot[1],180-oldRot[2],0)
-    roaches:moveTo(models.model.WORLD)
-  end
+local followedModelPart = head
+local roachHeadPos = roaches:partToWorldMatrix():apply()
+local roachHeadRot = player:getRot():mul(-1,-1,1):add(0,180,0)
+local followRoach = keybinds:newKeybind("Follow the roach toggle", "key.keyboard.u", false)
+local forward = keybinds:newKeybind("Move forward", "key.keyboard.y", false)
+local back = keybinds:newKeybind("Move backwards", "key.keyboard.h", false)
+local left = keybinds:newKeybind("Move left", "key.keyboard.g", false)
+local right = keybinds:newKeybind("Move right", "key.keyboard.j", false)
+local up = keybinds:newKeybind("Move up", "key.keyboard.b", false)
+local down = keybinds:newKeybind("Move down", "key.keyboard.n", false)
+followRoach:setOnPress(function()
+    roachHeadPos = roaches:partToWorldMatrix():apply()
+    roachHeadRot = player:getRot():mul(-1,-1,1):add(0,180,0)
+    if isFollowing then
+      followedModelPart = roaches
+    else
+      followedModelPart = head
+    
 end)
 function events.tick()
-  if secKey:isPressed() then
-    xOffset = xOffset + 2
-  end
-  if thirdKey:isPressed() then
-    xOffset = xOffset - 2
-  end
-  if fourKey:isPressed() then
-    zOffset = zOffset - 2
-  end
-  if fiveKey:isPressed() then
-    zOffset = zOffset + 2
-  end
-  if sixKey:isPressed() then
-    yOffset = yOffset - 2
-  end
-  if sevenKey:isPressed() then
-    yOffset = yOffset + 2
-  end
-  if math.abs(xOffset) > 0.5 then
-    xOffset = xOffset - 0.1*xOffset
-  else
-    xOffset = 0
-  end
-  if math.abs(yOffset) > 0.5 then
-    yOffset = yOffset - 0.1*yOffset
-  else
-    yOffset = 0
-  end
-  if math.abs(zOffset) > 0.2 then
-    zOffset = zOffset - 0.1*zOffset
-  else
-    zOffset = 0
-  end
-  if not roach then
-  sPos = roaches:getPos()
-  curPos = roaches:getPos():add(vec(xOffset,yOffset,zOffset):mul(player:getLookDir()))
-  end
+  
 end
 
 
 function events.post_render(delta)
-  if not roach then
-    roaches:setPos(math.lerp(sPos,curPos,delta))
-    roaches:setRot(-player:getRot()[1],180-player:getRot()[2],0)
-    renderer:setCameraPivot(math.lerp(sPos,curPos,delta):div(vec(16,16,16)):add(vec(0,2,0)))
-  else
-    renderer:setOffsetCameraPivot(vec(0,0,0))
-    --renderer:setCameraPos(0,0,0)
-  end
+    if followedModelPart then
+        local pos = followedModelPart:partToWorldMatrix():apply():mul(16,16,16)
+        local rot = player:getRot()
+        followedModelPart:setPos(pos)
+        followedModelPart:setRot(-rot[1],180-rot[2],0)
+    end
 end
