8249079d0b01a2fe2c983dff5f7a5ed15dce13bb
Chanied <132589948+WolfZoned@users.noreply.github.com>
2024-07-23 10:44:45 -0400

    Co-authored-by: asosuhuj344@gmail.com <asosuhuj344@gmail.com>

--- Misc/scoreLink.lua
+++ Misc/scoreLink.lua
@@ -1,4 +1,6 @@
 local modem = peripheral.find("modem")
+local basalt = require("basalt")
+local main = basalt.createFrame()
 local scoreLinkBroadcast = 52964
 local targetDestination = 0 --ID of the computer you are connecting to
 print("true for source, false for target")
@@ -6,31 +8,85 @@ local source = read() -- true or false for source or target
 name = "test1"
 computerID = os.getComputerID()
 local sourceDestination, message, protocol
-local targetList = {} --list of all the conections that are currently open
+local openList = {} --list of all the conections that are currently open
 --{targetDestination = {source, name}}
-
+local connectionList = {} --list of all the connections that have been made
+--{targetDestination = {source, name}}
+local searching = true --is the computer searching for a connection?
+local curpos = 0
+local update = false
 
 
 function recieve()
-    local recieved = {}
     while true do
-        if targetDestination == 0 then
-            recieved = rednet.receive("scoreLinkBroadcast", 3)
-            if (not (recieved[2])[1] == source) and (not recieved[1] == computerID) then
-                sourceDestination, message, protocol = recieved[1], recieved[2], recieved[3]
-                targetList[sourceDestination] = message
+        if searching == true then
+            sourceDestination, message, protocol = rednet.receive("scoreLinkBroadcast")
+            if (not message[1] == source) and (not sourceDestination == computerID) then
+                openList[sourceDestination] = message
+                update = true
+                print("recieved")
             end
         end
     end
 end
 
 function send()
-    rednet.send(52964, {source, name}, "scoreLinkBroadcast")
+    rednet.broadcast({source, name}, "scoreLinkBroadcast")
 end
 
+function displayOpenConnections()
+    print("Open Connections:")
+    curpos = 1
+    for key, list in pairs(openList) do
+        curpos = curpos + 1
+        term.setCursorPos(1, curpos)
+        term.write(list[2])
+        term.setCursorPos(51-#(list[1]), curpos)
+        term.write(list[1])
+    end
+end
 function main()
-    
+    local pingTimer = os.startTimer(5)
+    local sendTimerFinish = false
+    parallel.waitForAny(function()--basalt:Threadorsomething("")
+        local event, id
+        while true do
+            event, id = os.pullEvent("timer")
+            if id == pingTimer then
+                sendTimerFinish = true
+                pingTimer = os.startTimer(5)
+                print("timer")
+            end
+        end
+    end,
+    function()
+        while true do
+            if update == true then
+                displayOpenConnections()
+                update = false
+                print("updated")
+            end
+            if searching == true and sendTimerFinish == true then
+                send()
+                sendTimerFinish = false
+                print("sent")
+            end
+            sleep(0.5)
+        end
+    end)
 end
 
-rednet.open(modem)
+rednet.open(peripheral.getName(modem))
 parallel.waitForAll(main, recieve)
+
+basalt.autoUpdate()
+----------------------------------------------------------------------------------------------------
+--https://basalt.madefor.cc/#/objects/Thread
+local basalt = require("basalt")
+local main = basalt.createFrame()
+local modem = peripheral.find("modem")
+rednet.open(modem)
+function update()
+    rednet.broadcast({})
+end
+basalt.autoUpdate()
