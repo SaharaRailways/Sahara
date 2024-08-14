ac32ae648431faf8e1b7f810df74cc59ece59ca2
Chanied <132589948+WolfZoned@users.noreply.github.com>
2024-08-13 20:07:20 -0400

    added more functions to catchAPI and basically finished it

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # Sahara
 
 
-Any use of our code is phohibited 
+This code is free to use anywhere outside of the Steam 'n' Rails season 2 server
+Distributed versions of this code is also not allowed to be used in the Steam 'n' Rails server
+tbh i haven't read the liscense so what i just said takes priority
--- common/catchAPI.lua
+++ common/catchAPI.lua
@@ -1,39 +1,72 @@
-catch = {}
+local catch = {}
 
---you haven't tested the listen start and stop part yet!
---do that
---now
---you don't have a choice lol
-
-function catch.setup(mainThread)
-    catchData = {}
-    catchDataTemp = {}
-    listenFor = {}
-    parallel.waitForAny(catch.listen, mainThread)
-end
-
+-- Internal function to listen for events
 function catch.listen()
-    --don't call this function in your own code
     while true do
-        catchDataTemp = {os.pullEvent()}
+        local catchDataTemp = {os.pullEvent()}
         print("catchDataTemp: " .. catchDataTemp[1])
-        if listenFor[catchDataTemp[1]] then
+        if listenFor[catchDataTemp[1]] or listenFor["all"] then
             if catchData[catchDataTemp[1]] == nil then
                 catchData[catchDataTemp[1]] = {}
             end
-            table.insert(catchData[catchDataTemp[1]], catchDataTemp)
-        elseif catchDataTemp[1] == "startListen" then
-            listenFor[catchDataTemp[2]] = true
-        elseif catchDataTemp[1] == "stopListen" then
-            listenFor[catchDataTemp[2]] = false
+            table.insert(catchData[catchDataTemp[1]], 1, {catchDataTemp, os.clock()})
         end
     end
 end
 
-function catch.add()
-    
+-- Call this function to start listening for an event
+function catch.add(eventName)
+    listenFor[eventName] = true
+end
+
+--Call this function to start listening for all events
+--To stop listening for all events, call catch.remove("all")   -this will not delete the other listenFor entries, only prevent every event from being caught
+function catch.addAll()
+    listenFor["all"] = true
+end
+
+-- Call this function to stop listening for an event
+function catch.remove(eventName)
+    listenFor[eventName] = false
+end
+
+--Call this function to stop listening to any events spcified by catch.add()
+function catch.removeAll()
+    listenFor = {}
+end
+
+-- Provide the name of the script you want to run in parallel to the catchAPI (just put the name of the main function)
+function catch.setup(mainThreadName)
+    catchData = {}
+    catchDataTemp = {}
+    listenFor = {}
+
+    -- Get the function from the global table using its name
+    local mainThread = _G[mainThreadName]
+
+    -- Check if the function exists
+    if type(mainThread) == "function" then
+        parallel.waitForAny(catch.listen, mainThread)
+    else
+        error("Function " .. mainThreadName .. " does not exist")
+    end
+end
+
+-- An oversimplified way to get the data from the catchData table
+function catch.getData(eventName)
+    return catchData[eventName] or {}
+end
+
+-- An oversimplified way to clear the data from the catchData table
+function catch.clearData(eventName)
+    if eventName then
+        catchData[eventName] = nil
+    else
+        catchData = {}
+    end
 end
 
 return catch
 
---function mainThread() while true do sleep(1) os.queueEvent("test") end end
+-- Example main thread function
+-- function mainThread() while true do sleep(1) os.queueEvent("test") end end
